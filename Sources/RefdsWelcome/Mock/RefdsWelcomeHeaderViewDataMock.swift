3ac8cfb1f0b6961bddb3fe9c00cd99438cd10dd8
Rafael Escaleira <rafaelescaleira@icloud.com>
2024-05-11 21:15:26 -0400

    Update RefdsWelcomeHeaderViewDataMock.swift

--- Sources/RefdsWelcome/Mock/RefdsWelcomeHeaderViewDataMock.swift
+++ Sources/RefdsWelcome/Mock/RefdsWelcomeHeaderViewDataMock.swift
@@ -3,13 +3,10 @@ import RefdsUI
 import RefdsShared
 
 public struct RefdsWelcomeHeaderViewDataMock: RefdsWelcomeHeaderViewDataProtocol {
+    public var applicationIcon: Image = RefdsAsset.applicationIcon.image
     public var introduceTitle: String = .refdsLocalizable(by: .welcomeIntroduction)
     public var applicationTitle: String = .someWord()
     public var description: String? = .someParagraph()
-    
-    public var applicationIcon: Image? {
-        RefdsAsset.applicationIcon.image
-    }
     
     public init() {}
 }
